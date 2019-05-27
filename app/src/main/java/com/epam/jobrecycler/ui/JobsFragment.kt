8ad78e800440baa773124f2661b017a4b4b19a65
Vlad Korotkevich <vlad.korotkevich2@gmail.com>
2019-05-27 19:21:19 +0300

    Check for connection added.

--- app/src/main/java/com/epam/jobrecycler/ui/JobsFragment.kt
+++ app/src/main/java/com/epam/jobrecycler/ui/JobsFragment.kt
@@ -1,5 +1,10 @@
 package com.epam.jobrecycler.ui
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -22,6 +27,34 @@ class JobsFragment : Fragment() {
 
     private val _jobs: MutableList<Job> = mutableListOf()
     val jobs: List<Job> = _jobs
+
+    private lateinit var connectivityManager: ConnectivityManager
+    private var isDownloaded = false
+
+    private val networkRequest =
+        NetworkRequest.Builder().addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI).build()
+
+    private val connectivityCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network?) {
+            super.onAvailable(network)
+            if (isDownloaded) return
+            Toast.makeText(context, getString(R.string.retrying_download), Toast.LENGTH_SHORT).show()
+            val recyclerView = view?.findViewById<RecyclerView>(R.id.jobsRecycler)
+            loadJobs(recyclerView?.adapter, view?.findViewById(R.id.progressBar))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.registerNetworkCallback(networkRequest, connectivityCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        connectivityManager.unregisterNetworkCallback(connectivityCallback)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,19 +79,20 @@ class JobsFragment : Fragment() {
         }
     }
 
-    private fun loadJobs(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?, progressBar: ProgressBar) {
+    private fun loadJobs(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?, progressBar: ProgressBar?) {
         val call: Call<List<Job>> = NetworkService.retrofit.getJobs()
         call.enqueue(object : Callback<List<Job>> {
             override fun onFailure(call: Call<List<Job>>, t: Throwable) {
-                Toast.makeText(context, "Failed downloading jobs...", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, getString(R.string.faild_to_download), Toast.LENGTH_LONG).show()
                 Log.e(TAG, t.toString())
             }
 
             override fun onResponse(call: Call<List<Job>>, response: Response<List<Job>>) {
                 if (response.isSuccessful) {
-                    progressBar.visibility = View.GONE
+                    progressBar?.visibility = View.GONE
                     _jobs.addAll(response.body() as List<Job>)
                     adapter?.notifyDataSetChanged()
+                    isDownloaded = true
                 }
             }
 
